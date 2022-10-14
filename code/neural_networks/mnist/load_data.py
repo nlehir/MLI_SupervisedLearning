f7ef4edfc8e749c4e8a3cb1a585e671cc8220509
nico <nicolaslehir@gmail.com>
2022-10-14 09:32:47 +0200

    clean mnist Some files were useless. To download the data, students can run load_data.py

--- code/neural_networks/mnist/load_data.py
+++ code/neural_networks/mnist/load_data.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-np.save("x_train", x_train)
-np.save("y_train", y_train)
-np.save("x_test", x_test)
-np.save("y_test", y_test)
+np.save("data/x_train", x_train)
+np.save("data/y_train", y_train)
+np.save("data/x_test", x_test)
+np.save("data/y_test", y_test)
