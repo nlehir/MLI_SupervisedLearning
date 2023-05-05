19e9258cef46204835efaae89dca9605a514040a
nico <nicolaslehir@gmail.com>
2023-05-05 18:43:37 +0200

    update show_digits.py

--- code/logistic_regression/digits/show_digit.py
+++ code/logistic_regression/digits/show_digit.py
@@ -3,8 +3,12 @@ from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
 
 
-sample_index = 12
-digits = load_digits()
-plt.imshow(digits.data[sample_index].reshape(8, 8))
-figpath = os.path.join("images", f"sample_{sample_index}.pdf")
-plt.savefig(figpath,)
+sample_indexes = [1, 2, 3, 12, 100]
+for sample_index in sample_indexes:
+    digits = load_digits()
+    targets = digits.target
+    plt.imshow(digits.data[sample_index].reshape(8, 8))
+    title = f"label: {targets[sample_index]}"
+    plt.title(title)
+    figpath = os.path.join("images", f"sample_{sample_index}.pdf")
+    plt.savefig(figpath)
