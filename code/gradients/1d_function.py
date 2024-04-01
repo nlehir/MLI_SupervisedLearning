88767ae65d27302a1200f2b727c7424179503fb3
Nicolas Le Hir <nicolaslehir@gmail.com>
2024-04-01 18:15:20 +0200

    update gradients 1d_function.py and pdf

--- code/gradients/1d_function.py
+++ code/gradients/1d_function.py
@@ -1,45 +1,63 @@
 import os
-from typing import Union
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+X_START = 10
+GAMMA = 0.01
+N_ITER = int(2e2)
 
-def func(x: float) -> float:
+def func(x):
     return (x-1)**2 +3.5
 
-def func_derivative(x: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+def func_derivative(x):
     return 2 * (x-1)
 
 
-def main():
-    n_iter = int(1e3)
-    x_start = 6
-    x = x_start
-    gamma = 0.1
-    x_list = list()
-    y_list = list()
-    x_list.append(x)
-    y_list.append(func(x))
-    for it in range(n_iter):
+def main() -> None:
+
+    min_x = X_START
+    max_x = X_START
+
+    # perform gradient descent
+    bbox = dict(boxstyle="circle", fc="b", alpha=0.2)
+    x = X_START
+    for it in range(N_ITER):
         print(f"iter: {it}")
+        y = func(x)
+        # plot the current iterate
+        plt.annotate(text=f"{it}", xy=(x,y), bbox=bbox)
         # gradient update
-        x = x - gamma * func_derivative(x)
-        x_list.append(x)
-        y_list.append(func(x))
+        x = x - GAMMA * func_derivative(x)
         print(f"x: {x}")
         print(f"f(x): {func(x)}")
 
-    xx = np.linspace(-10, 10)
+        # update the plot range
+        if x > max_x:
+            max_x = x
+        if x < min_x:
+            min_x = x
+
+
+    # plot a little wider
+    min_x -= 2
+    max_x += 2
+    # plot the function on a regular grid
+    xx = np.linspace(min_x, max_x)
     yy = func(xx)
-    plt.plot(xx, yy)
-    plt.plot(x_list, y_list, "o", label="GD iterates", alpha=0.8, markersize=5)
+    plt.plot(xx, yy, label="function")
+
+    # finish plot
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend(loc="best")
-    title=f"gradient descent\ngamma={gamma}"
+    title=(
+            f"gradient descent"
+            f"\ngamma={GAMMA}"
+            f"\nx_0={X_START}"
+            )
     plt.title(title)
-    figpath = os.path.join("1d_function", f"function_gd_gamma_{gamma}.pdf")
+    figpath = os.path.join("1d_function", f"gd_gamma_{GAMMA}_x0_{X_START:.1E}.pdf")
     plt.savefig(figpath)
     plt.close()
 
