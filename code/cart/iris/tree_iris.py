775f793a9c15f4ecc244ac9ac419fc8496ce7b0c
nlehir <nicolaslehir@gmail.com>
2024-10-11 01:07:15 +0200

    update cart/iris

--- code/cart/iris/tree_iris.py
+++ code/cart/iris/tree_iris.py
@@ -5,10 +5,22 @@ import graphviz
 
 
 def main() -> None:
+    """
+    Add some lines here, in order to learn a classification tree, with pruning,
+    on the iris dataset.
+
+    In order to extract the data from the load_iris() function, we can
+    use the relevant attributes, as explained in the docs
+    https://scikit-learn.org/1.5/modules/generated/sklearn.datasets.load_iris.html#sklearn.datasets.load_iris
+
+    You can also experiment with other datasets, like the other scikit toy datasets
+    https://scikit-learn.org/1.5/datasets/toy_dataset.html
+    """
     iris = load_iris()
-    """
-    Add some lines here
-    """
+
+    X = iris.data
+    y = iris.target
+
 
 if __name__ == "__main__":
     main()
